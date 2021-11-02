e8677f2308bac9ceaff52de6c2dc838b1e43116b
Slaviano-x <simapp.ru@gmail.com>
2021-11-02 14:07:49 +0700

    update

--- app/src/main/java/space/tyryshkin/jkarta/Activity_General_Space_App.java
+++ app/src/main/java/space/tyryshkin/jkarta/Activity_General_Space_App.java
@@ -1,19 +1,121 @@
 package space.tyryshkin.jkarta;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class Activity_General_Space_App extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class Activity_General_Space_App extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+    ActionBarDrawerToggle toggle;
+    NavigationView navigationView;
+    private Toolbar toolbar;
+    private final FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general_space_app);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        createNavigationDrawer();
+    }
+
+    private void createNavigationDrawer() {
+        drawer = findViewById(R.id.drawer_layout);
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        changeFragment(new Fragment_General_Home(), true);
+        navigationView.setCheckedItem(R.id.nav_home);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                changeFragment(new Fragment_General_Home(), true);
+                break;
+            case R.id.nav_cards:
+                changeFragment(new Fragment_General_Cards(), true);
+                break;
+            case R.id.nav_mcc:
+                changeFragment(new Fragment_General_Settings(), true);
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void changeFragment(Fragment fragment, boolean needToAddBackStack) {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment, fragment.getClass().getName());
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        if (needToAddBackStack) {
+            fragmentTransaction.addToBackStack(fragment.getTag());
+        }
+        fragmentTransaction.commit();
     }
 
     @Override
     public void onBackPressed() {
-//Должно быть пустым!!!
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else if (fragmentManager.getBackStackEntryCount() > 1) {
+            fragmentManager.popBackStack();
+
+            int index = fragmentManager.getBackStackEntryCount() - 2;
+            FragmentManager.BackStackEntry backStackEntry = getSupportFragmentManager().getBackStackEntryAt(index);
+            String tag = backStackEntry.getName();
+            Fragment fragment = (Fragment) getSupportFragmentManager().findFragmentByTag(tag);
+            if (fragment instanceof Fragment_General_Home) {
+                navigationView.setCheckedItem(R.id.nav_home);
+            } else if (fragment instanceof Fragment_General_Cards) {
+                navigationView.setCheckedItem(R.id.nav_cards);
+            } else if (fragment instanceof Fragment_General_Settings) {
+                navigationView.setCheckedItem(R.id.nav_mcc);
+            }
+        } else {
+            finishAndRemoveTask();
+        }
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        if (toggle != null) {
+            toggle.syncState();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.fragment_container) {
+            if (drawer.isDrawerOpen(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START);
+            } else {
+                drawer.openDrawer(GravityCompat.START);
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/space/tyryshkin/jkarta/Activity_SignIn.java
+++ app/src/main/java/space/tyryshkin/jkarta/Activity_SignIn.java
@@ -2,20 +2,47 @@ package space.tyryshkin.jkarta;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.graphics.drawable.DrawableCompat;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Patterns;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class Activity_SignIn extends AppCompatActivity {
 
@@ -27,6 +54,10 @@ public class Activity_SignIn extends AppCompatActivity {
     private DatabaseReference userDataBase;
     private String USER_KEY = "users";
 
+    public Activity_SignIn instance;
+
+    private ArrayList<String> listOfEmail = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +68,8 @@ public class Activity_SignIn extends AppCompatActivity {
     }
 
     public void init() {
+        instance = this;
+
         email_layout = findViewById(R.id.email_layout);
         password_layout = findViewById(R.id.password_layout);
         email_edit = findViewById(R.id.email_edit);
@@ -47,6 +80,8 @@ public class Activity_SignIn extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         userDataBase = FirebaseDatabase.getInstance().getReference(USER_KEY);
+
+        listOfEmail = findAllEmail();
     }
 
     private void onClicks() {
@@ -65,6 +100,8 @@ public class Activity_SignIn extends AppCompatActivity {
                                     if (task.isSuccessful()) {
                                         Intent intent = new Intent(getApplicationContext(), Activity_General_Space_App.class);
                                         startActivity(intent);
+                                    } else {
+                                        password_layout.setError("Неверный Email или пароль");
                                     }
                                 }
                             });
@@ -82,7 +119,76 @@ public class Activity_SignIn extends AppCompatActivity {
         btn_restore_password.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                showDialogResetPassword();
+            }
+        });
+    }
 
+    private void showDialogResetPassword() {
+        Dialog dialog = createDialog(R.layout.dialog_reset_password);
+
+        placeDialogBottom(dialog);
+
+        TextInputLayout email_layout = dialog.findViewById(R.id.email_layout);
+        TextInputEditText edit = dialog.findViewById(R.id.edit);
+        MaterialButton cancel = dialog.findViewById(R.id.btn_cancel);
+        MaterialButton next = dialog.findViewById(R.id.btn_next);
+
+        next.setOnClickListener(view -> {
+            onChanges(email_layout, edit, next);
+            String mail = edit.getText().toString();
+
+            if (checkValidateEmail2(email_layout, edit, next)) {
+                mAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        dialog.dismiss();
+                        hideKeyBoard();
+                        Toast toast = Toast.makeText(Activity_SignIn.this, getResources().getString(R.string.reset_password_toast), Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.TOP, 0, 400);
+                        View view = toast.getView();
+                        view.getBackground().setColorFilter(getColor(R.color.white), PorterDuff.Mode.SRC_IN);
+                        TextView text = view.findViewById(android.R.id.message);
+                        text.setTextColor(getColor(R.color.teal_500));
+                        text.setGravity(Gravity.CENTER);
+                        toast.show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast toast = Toast.makeText(Activity_SignIn.this, getResources().getString(R.string.failure), Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.TOP, 0, 400);
+                        View view = toast.getView();
+                        view.getBackground().setColorFilter(getColor(R.color.white), PorterDuff.Mode.SRC_IN);
+                        TextView text = view.findViewById(android.R.id.message);
+                        text.setTextColor(getColor(R.color.error));
+                        text.setGravity(Gravity.CENTER);
+                        toast.show();
+                    }
+                });
+            }
+        });
+
+        cancel.setOnClickListener(view -> dialog.dismiss());
+
+        dialog.show();
+    }
+
+    private void onChanges(TextInputLayout email_layout, EditText email_edit, MaterialButton next) {
+        email_edit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (checkValidateEmail2(email_layout, email_edit, next)) {
+                    next.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
             }
         });
     }
@@ -92,11 +198,30 @@ public class Activity_SignIn extends AppCompatActivity {
         if (email.equals("")) {
             email_layout.setError("Поле обязательно для заполнения");
             return false;
-        } else if (!email.contains("@")) {
+        } else if (!isEmailValid(email)) {
             email_layout.setError("Пример: mail@example.com");
             return false;
         } else {
             email_layout.setError(null);
+            return true;
+        }
+    }
+    private boolean checkValidateEmail2(TextInputLayout email_layout, EditText edit, MaterialButton next) {
+        if (Objects.requireNonNull(edit.getText()).toString().equals("")) {
+            email_layout.setError("Поле обязательно для заполнения");
+            next.setEnabled(false);
+            return false;
+        } else if (!isEmailValid(edit.getText().toString())) {
+            email_layout.setError("Пример: mail@example.com");
+            next.setEnabled(false);
+            return false;
+        } else if (!containsIgnoreCase(listOfEmail, edit.getText().toString())) {
+            email_layout.setError("Email не зарегистрирован");
+            next.setEnabled(false);
+            return false;
+        } else {
+            email_layout.setError(null);
+            next.setEnabled(true);
             return true;
         }
     }
@@ -105,17 +230,71 @@ public class Activity_SignIn extends AppCompatActivity {
         if (password.equals("")) {
             password_layout.setError("Поле обязательно для заполнения");
             return false;
-        } else if (password.length() < 6) {
-            password_layout.setError("Пароль должен состоять минимум из 6 символов");
-            return false;
         } else {
             password_layout.setError(null);
             return true;
         }
     }
 
+    private boolean isEmailValid(CharSequence email) {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private Dialog createDialog(int layout) {
+        Dialog dialog = new Dialog(Activity_SignIn.this);
+        dialog.setContentView(layout);
+        dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.fon_with_margin));
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.setCancelable(false);
+
+        return dialog;
+    }
+    private void placeDialogBottom(Dialog dialog) {
+        Window window = dialog.getWindow();
+        WindowManager.LayoutParams windowParams = window.getAttributes();
+
+        windowParams.gravity = Gravity.BOTTOM;
+        window.setAttributes(windowParams);
+    }
+
+    private ArrayList<String> findAllEmail() {
+        userDataBase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    String email = String.valueOf(ds.child("email").getValue());
+                    listOfEmail.add(email);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return listOfEmail;
+    }
+
+    private boolean containsIgnoreCase(ArrayList<String> list, String string) {
+        for (String i : list) {
+            if (i.equalsIgnoreCase(string)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     public void onBackPressed() {
 //Должно быть пустым!!!
+    }
+
+    public void hideKeyBoard() {
+        View keyBoard = instance.getCurrentFocus();
+        if (keyBoard != null) {
+            InputMethodManager inputMethodManager = (InputMethodManager) instance.getSystemService(INPUT_METHOD_SERVICE);
+            assert inputMethodManager != null;
+            inputMethodManager.hideSoftInputFromWindow(keyBoard.getWindowToken(), 0);
+        }
     }
 }
